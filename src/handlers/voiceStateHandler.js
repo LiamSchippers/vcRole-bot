2c01e52507b9ee782bca7b4b5a2de7ad655bda54
Liam Schippers <liam.s166@gmail.com>
2022-10-19 21:29:10 +0200

    onChannel change fix

--- src/handlers/voiceStateHandler.js
+++ src/handlers/voiceStateHandler.js
@@ -23,6 +23,18 @@ export default class VoiceStateHandler {
 		}
 		else {
 			console.log('user moved channels', oldState.channelId, newState.channelId);
+
+			let role = this.vcRoles.getRole(newState.channelId);
+
+			if (role != null) {
+				newState.member.roles.add(role);
+				return;
+			}
+
+			role = this.vcRoles.getRole(oldState.channelId);
+			if (role != null) {
+				newState.member.roles.remove(role);
+			}
 		}
 	}
 }
